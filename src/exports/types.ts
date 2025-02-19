c2682fb2f21b7d1ade5104ee7294f72fe90da7af
FemiBlack <tadefemi2@hotmail.com>
2025-02-19 11:17:33 +0100

    chore: add types to exports

---
+++ src/exports/types.ts
@@ -0,0 +1,10 @@
+export type {
+  FieldSyncConfig,
+  PaystackEvent,
+  PaystackPluginConfig,
+  PaystackProxy,
+  PaystackWebhookHandler,
+  PaystackWebhookHandlers,
+  SanitizedPaystackPluginConfig,
+  SyncConfig,
+} from '../types.js'
